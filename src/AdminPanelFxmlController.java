729b7ef79d8f9da0fd41771b1bc04f998ea2199e
Shahriar Saikat <saikatbass@gmail.com>
2022-03-21 15:25:54 +0600

    Design Update by Saikat

--- src/AdminPanelFxmlController.java
+++ src/AdminPanelFxmlController.java
@@ -1,25 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
-/**
- * FXML Controller class
- *
- * @author Hp
- */
+
 public class AdminPanelFxmlController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Label searchLabel;
+    @FXML
+    private TextField searchTF;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Label infoLabel;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private Label doseOneLabel;
+    @FXML
+    private ComboBox<?> doseOneComboBox;
+    @FXML
+    private Label dateOneLabel;
+    @FXML
+    private DatePicker datePickerOne;
+    @FXML
+    private Label doseTwoLabel;
+    @FXML
+    private ComboBox<?> doseTwoComboBox;
+    @FXML
+    private Label dateTwoLabel;
+    @FXML
+    private DatePicker datePickerTwo;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void adminSearchBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void adminUpdateBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void adminLogoutBtnAction(ActionEvent event) {
+    }
     
 }
--- src/IntroPageFXMLController.java
+++ src/IntroPageFXMLController.java
@@ -1,11 +1,14 @@
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 
 public class IntroPageFXMLController implements Initializable {
@@ -28,7 +31,8 @@ public class IntroPageFXMLController implements Initializable {
 
     @FXML
     private void adminLogInBtnAction(ActionEvent event) {
-        System.out.println("Admin Panel Working");
+        System.out.println("ssss");
+        
     }
     
 }
