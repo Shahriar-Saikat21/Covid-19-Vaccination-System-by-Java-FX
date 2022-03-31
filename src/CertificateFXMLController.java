4bb8dd1dc48c4d81ed84ba14b4ea21b2268981b7
Shahriar Saikat <saikatbass@gmail.com>
2022-03-31 10:13:28 +0600

    UI Design Finalize

--- src/CertificateFXMLController.java
+++ src/CertificateFXMLController.java
@@ -5,52 +5,63 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CertificateFXMLController implements Initializable{
 
     @FXML
-    private Label certificateDateofBirth;
+    private Button certificateSendOtpBtn;
 
     @FXML
-    private Label certificateEmailAddress;
+    private ComboBox<String> comboBoxDate;
 
     @FXML
-    private Label certificateOTP;
+    private ComboBox<String> comboBoxMonth;
 
     @FXML
-    private Label certificateYourNID;
+    private ComboBox<String> comboBoxYear;
 
     @FXML
-    private Button certificatesentOTPbtn;
+    private TextField mailTF;
 
     @FXML
-    private Button sendcertificatebtn;
-    
-    @FXML
-    private ComboBox<String> comboboxdate;
+    private TextField nidTF;
 
     @FXML
-    private ComboBox<String> comboboxmonth;
+    private TextField otpTF;
 
     @FXML
-    private ComboBox<String> comboboxyear;
+    private Button sendCertificateBtn;
 
     @FXML
-    void certificatesentOTPAction(ActionEvent event) {
-
+    void certificateSendOtpBtnAction(ActionEvent event) {
+        System.out.println("Certificate OTP Working");
     }
 
     @FXML
-    void sendcertificateAction(ActionEvent event) {
-
+    void sendCertificateBtnAction(ActionEvent event) {
+        System.out.println("Certificate Send Working");
     }
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        comboboxdate.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31");
-        comboboxmonth.getItems().addAll("January","February","March","April","May","June","July","August","September","October","November","December");
-        comboboxyear.getItems().addAll("1920","1921","1922","1923","1924","1925","1926","1927","1928","1929","1930","1931","1932","1933","1933","1934","1935","1936","1937","1938","1939","1940","1941","1942","1943","1944","1945","1946","1947","1948","1949","1950","1951","1952","1953","1954","1955","1956","1957","1958","1959","1960","1961","1962","1963","1964","1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022");
+        comboBoxDate.getItems().addAll("01","02","03","04","05","06","07","08","09","10",
+                "11","12","13","14","15","16","17","18","19","20","21","22","23","24",
+                "25","26","27","28","29","30","31");
+        comboBoxMonth.getItems().addAll("January","February","March","April","May",
+                "June","July","August","September","October","November","December");
+        comboBoxYear.getItems().addAll("2022","2021","2020","2019","2018","2017","2016",
+                "2015","2014","2013","2012","2011","2010","2009","2008","2007",
+                "2006","2005","2004","2003","2002","2001","2000","1999","1998","1997",
+                "1996","1995","1994","1993","1992","1991","1990","1989","1988","1987",
+                "1986","1985","1984","1983","1982","1981","1980","1979","1978","1977",
+                "1976","1975","1974","1973","1972","1971","1970","1969","1968","1967",
+                "1966","1965","1964","1963","1962","1961","1960","1959","1958","1957",
+                "1956","1955","1954","1953","1952","1951","1950","1949","1948","1947",
+                "1946","1945","1944","1943","1942","1941","1940","1939","1938","1937",
+                "1936","1935","1934","1933","1933","1932","1931","1930","1929","1928",
+                "1927","1926","1925","1924","1923","1922","1921","1920");
     }
 
 }
--- src/VaccineCardFXMLController.java
+++ src/VaccineCardFXMLController.java
@@ -5,24 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class VaccineCardFXMLController implements Initializable{
-
-    @FXML
-    private Label certificateDateofBirth;
-
-    @FXML
-    private Label certificateEmailAddress;
-
-    @FXML
-    private Label certificateOTP;
-
-    @FXML
-    private Label certificateYourNID;
-
-    @FXML
-    private Button certificatesentOTPbtn;
 
     @FXML
     private ComboBox<String> comboboxdate;
@@ -34,23 +19,47 @@ public class VaccineCardFXMLController implements Initializable{
     private ComboBox<String> comboboxyear;
 
     @FXML
-    private Button sendcertificatebtn;
+    private TextField mailTF;
 
     @FXML
-    void certificatesentOTPAction(ActionEvent event) {
+    private TextField nidTF;
 
+    @FXML
+    private TextField otpTF;
+
+    @FXML
+    private Button sendVaccineCardbtn;
+
+    @FXML
+    private Button vaccineCardOtpBtn;
+
+    @FXML
+    void sendVaccineCardbtnAction(ActionEvent event) {
+        System.out.println("Send VC Working");
     }
 
     @FXML
-    void sendcertificateAction(ActionEvent event) {
-
+    void vaccineCardOtpBtnAction(ActionEvent event) {
+        System.out.println("VC OTP Working");
     }
  
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        comboboxdate.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31");
-        comboboxmonth.getItems().addAll("January","February","March","April","May","June","July","August","September","October","November","December");
-        comboboxyear.getItems().addAll("1920","1921","1922","1923","1924","1925","1926","1927","1928","1929","1930","1931","1932","1933","1933","1934","1935","1936","1937","1938","1939","1940","1941","1942","1943","1944","1945","1946","1947","1948","1949","1950","1951","1952","1953","1954","1955","1956","1957","1958","1959","1960","1961","1962","1963","1964","1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022");
+        comboboxdate.getItems().addAll("01","02","03","04","05","06","07","08",
+                "09","10","11","12","13","14","15","16","17","18","19","20",
+                "21","22","23","24","25","26","27","28","29","30","31");
+        comboboxmonth.getItems().addAll("January","February","March","April",
+                "May","June","July","August","September","October","November","December");
+        comboboxyear.getItems().addAll("2022","2021","2020","2019","2018","2017","2016","2015",
+                "2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003"
+                ,"2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992",
+                "1991","1990","1989","1988","1987","1986","1985","1984","1983","1982","1981",
+                "1980","1979","1978","1977","1976","1975","1974","1973","1972",
+                "1971","1970","1969","1968","1967","1966","1965","1964","1963",
+                "1962","1961","1960","1959","1958","1957","1956","1955","1954","1953","1952",
+                "1951","1950","1949","1948","1947","1946","1945","1944","1943","1942","1941",
+                "1940","1939","1938","1937","1936","1935","1934","1933","1933","1932","1931",
+                "1930","1929","1928","1927","1926","1925","1924","1923","1922","1921","1920");
     }
 
 }
