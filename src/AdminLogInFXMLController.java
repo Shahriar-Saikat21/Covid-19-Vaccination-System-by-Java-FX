3e2795eba0472ebc9092b83c1db47c5ee7cde6c8
Shahriar Saikat <saikatbass@gmail.com>
2022-04-10 00:43:31 +0600

    ok

--- src/AdminLogInFXMLController.java
+++ src/AdminLogInFXMLController.java
@@ -51,37 +51,52 @@ public class AdminLogInFXMLController implements Initializable{
 
     
     @FXML
-    private void adminLogInOTPAction(ActionEvent event) {                
-        System.out.println("OTP Working");
-    }
-
-    @FXML
-    private void adminLogInAction(ActionEvent event) throws Exception{ 
+    private void adminLogInOTPAction(ActionEvent event) throws Exception{                
         Connection DBConnection = DataBaseConnection.connectDB();
         
         String userName = userNameTF.getText();
         String userPassword = passwordTF.getText();
         
-        String query = "select * from adminUsers where userName = BINARY ? and password = BINARY ?";
+        String query = "select mail from adminUsers where userName = BINARY ? and password = BINARY ?";
         PreparedStatement statement = DBConnection.prepareStatement(query);
         statement.setString(1, userName);
         statement.setString(2, userPassword);
-        ResultSet result = statement.executeQuery();        
+        ResultSet result = statement.executeQuery();
         
         if(result.next()==false){
-            System.out.println("Login Failed !!");
-        }else{            
-            Parent root = FXMLLoader.load(getClass().getResource("AdminPanelFXML.fxml"));             
-
-            Scene adminPanelScene = new Scene(root);
-            Stage stage = (Stage)logInButton.getScene().getWindow();
-            stage.setScene(adminPanelScene);
-            Image appLogo = new Image("image/AppLogo.png");
-            stage.getIcons().add(appLogo);
-            stage.setTitle("Admin Panel");
-            adminPanelScene.getStylesheets().add(getClass().getResource("adminPanelStyle.css").toExternalForm());
-            stage.show();
+            System.out.println("Not Found");
+        }else{
+            System.out.println(result.getString("mail"));
         }
+    }
+
+    @FXML
+    private void adminLogInAction(ActionEvent event) throws Exception{ 
+//        Connection DBConnection = DataBaseConnection.connectDB();
+//        
+//        String userName = userNameTF.getText();
+//        String userPassword = passwordTF.getText();
+//        
+//        String query = "select * from adminUsers where userName = BINARY ? and password = BINARY ?";
+//        PreparedStatement statement = DBConnection.prepareStatement(query);
+//        statement.setString(1, userName);
+//        statement.setString(2, userPassword);
+//        ResultSet result = statement.executeQuery();        
+        
+//        if(result.next()==false){
+//            System.out.println("Login Failed !!");
+//        }else{            
+//            Parent root = FXMLLoader.load(getClass().getResource("AdminPanelFXML.fxml"));             
+//
+//            Scene adminPanelScene = new Scene(root);
+//            Stage stage = (Stage)logInButton.getScene().getWindow();
+//            stage.setScene(adminPanelScene);
+//            Image appLogo = new Image("image/AppLogo.png");
+//            stage.getIcons().add(appLogo);
+//            stage.setTitle("Admin Panel");
+//            adminPanelScene.getStylesheets().add(getClass().getResource("adminPanelStyle.css").toExternalForm());
+//            stage.show();
+//        }
         
         
         
--- src/OTP.java
+++ src/OTP.java
@@ -1,4 +1,46 @@
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class OTP {
-    //To Do
+
+	public void sendOTP() {
+		//authentication info
+		final String username = "saikat.music.design@gmail.com";
+		final String password = "saikatsaikatsaikat";
+		String fromEmail = "saikat.music.design@gmail.com";
+		String toEmail = "saikatbass@gmail.com";
+		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username,password);
+			}
+		});
+		//Start our mail message
+		MimeMessage msg = new MimeMessage(session);
+		try {
+			msg.setFrom(new InternetAddress(fromEmail));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+			msg.setSubject("Subject Line");
+                        msg.setText("1111");
+			
+			Transport.send(msg);
+			System.out.println("Sent message");
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} 
+
+	}
+
 }
